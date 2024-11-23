b9977132648aa3f30499f08b1f60d075f71bc6d8
Julio Cesar de Oliveira Martins <julio.martins@codex.com.br>
2024-11-22 22:27:39 -0300

    Fix - spc-100: Atualização da ia para mes e ano

--- src/Conciliacao/conciliacao.py
+++ src/Conciliacao/conciliacao.py
@@ -3,15 +3,7 @@ from statsmodels.tsa.arima.model import ARIMA
 import numpy as np
 
 def prever_proximos_meses(df, order=(1,1,0)):
-    # # Garantir que o índice 'mes_referencia' está configurado corretamente
-    # df.set_index('mes_referencia', inplace=True)
-
-    df['data'] = pd.to_datetime(df['mes_referencia'].astype(str), format='%m')
-    df.set_index('data', inplace=True)
-    df.index = pd.date_range(start=df.index[0], periods=len(df), freq='MS')
-    
-    
-    # Escolher o modelo ARIMA com a ordem fornecida
+        # Escolher o modelo ARIMA com a ordem fornecida
     modelo = ARIMA(df['total_registros'], order=order)
     model_fit = modelo.fit()
 
@@ -30,15 +22,17 @@ def tratamentoDado(df):
 
     # Extrai o mês da coluna de datas
     df['mes_referencia'] = df['created_at'].dt.month
-    
+    # Extrai o ano da coluna de datas
+    df['ano_referencia'] = df['created_at'].dt.year
+
     # Agora podemos agrupar por mês e período (antes ou após o dia 15) e contar os registros (total_registros)
-    df_mensal = df.groupby(['mes_referencia']).agg(
+    df_mensal = df.groupby(['mes_referencia', 'ano_referencia']).agg(
         total_registros=('id_x', 'size')
     ).reset_index()
 
-    df_mensal = df_mensal[~df_mensal['mes_referencia'].isin([9, 10, 11, 12])]
+    df_anual = df_mensal[df_mensal['ano_referencia'] == 2024]
 
-    return df_mensal
+    return df_anual
 
 def previsao(df):
     df_tratado = tratamentoDado(df)
